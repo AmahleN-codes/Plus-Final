3930b945ed95abe4ab56714c6ae8cf0e970100b8
AmahleN-codes <amahle.s.ndlela@outlook.com>
2024-11-13 10:49:18 +0200

    Inject the forecast HTML from JavaScript

--- src/script.js
+++ src/script.js
@@ -55,4 +55,28 @@ function handleSearchSubmit(event) {
 let searchFormElelment = document.querySelector("#search-form");
 searchFormElelment.addEventListener("submit", handleSearchSubmit);
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Tue", "Wed", "Thurs", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">☀️</div>
+            <div class="weather-forecast-temperatures">
+              <div class="weather-forecast-temperature">
+                <strong>15°</strong>
+              </div>
+              <div class="weather-forecast-temperature">9°</div>
+            </div>
+        </div>`;
+  });
+
+  forecastElement.innerHTML = forecastHtml;
+}
 searchCity("Johannesburg");
+displayForecast();
